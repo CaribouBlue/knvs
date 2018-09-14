3ad1ff5aef3428eaf10fe76bc8378b5b2f833a14
Alec Draymore <Draymore26@gmail.com>
2018-09-13 20:40:41 -0400

    ignore build/

--- build/client/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-var _reactDom = require("react-dom");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _reactDom.render)(_react.default.createElement("div", null, "APP"), document.getElementById('app-anchor'));
