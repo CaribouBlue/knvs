96d118581c436cd899909ddc96865573b5eb5c34
Alec Draymore <Draymore26@gmail.com>
2018-11-07 23:01:00 -0500

    added component index

---
+++ src/client/components/index.js
@@ -0,0 +1,9 @@
+import Landing from './landing/Landing';
+import Router from './router/Router';
+import UpdateEventForm from './update-event-form/UpdateEventForm';
+
+export {
+  Landing,
+  Router,
+  UpdateEventForm,
+};
--- src/client/components/router/Routes.jsx
+++ src/client/components/router/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ArtistDash from '../../modules/artist-mods/artist-dash';
-import Landing from '../landing/Landing';
+import { Landing } from '../';
 
 const Routes = props => (
   <Switch>
